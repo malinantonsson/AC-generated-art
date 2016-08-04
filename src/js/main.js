e72e33c916eb16c53ba9b69e249098ab498d63ab
Malin Antonsson <Malin.Antonsson@rga.com>
2016-08-04 22:37:19 +0100

    Add binary clock

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,49 @@
+var binaryClock = {
+	runBinaryClock: function() {
+		var self = this;
+
+		var currentTime = new Date();
+		var hours = currentTime.getHours().toString(2);
+		var minutes = currentTime.getMinutes().toString(2);
+		var seconds = currentTime.getSeconds().toString(2);
+
+		var binaryH = this.addZero(6,hours);
+		var binaryM = this.addZero(6,minutes);
+		var binaryS = this.addZero(6,seconds);
+
+		this.setStatus(binaryH, 'hours');
+		this.setStatus(binaryM, 'minutes');
+		this.setStatus(binaryS, 'seconds');
+
+		window.setTimeout(function() {
+			self.runBinaryClock();
+		}, 1000);  
+	},
+ 	setStatus: function(binaryVal, rowPrefix) {
+		for (var i = 0; i < 6; i++) {
+			var val = binaryVal.substr(i,1);
+			var currentEl = document.getElementById(rowPrefix + '-' + i);
+			if (currentEl) {
+			  if (val == 0) {
+		  		currentEl.className = 'binaryOff';
+		  	  } else {
+				currentEl.className = 'binaryOn';	
+			  }
+			}
+		}
+	},
+	addZero: function(len, str) {
+		for (var i = str.length; str.length < len; i++) {
+			str = '0' + str;
+		}
+		return str;
+	}
+}
+
+
 var art = {
 	init: function() {
-		console.log('loaded!');
+		binaryClock.runBinaryClock();
 	}
 }
 
