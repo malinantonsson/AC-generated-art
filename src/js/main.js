82e944bc25d64c72cfd3f3f68d7cbef9f67ab3ad
Malin Antonsson <Malin.Antonsson@rga.com>
2016-08-06 22:43:59 +0100

    add wind

--- src/js/main.js
+++ src/js/main.js
@@ -48,13 +48,53 @@ var art = {
 	},
 
 	ui: {
-		body: document.getElementsByTagName('body')[0]
+		body: document.getElementsByTagName('body')[0],
+		office: document.getElementsByClassName('icon--plus')[0]
 	}, 
 
 	settings: {
 		weatherApiKey: '18b3eeda7e71d2d3946ccfbfeea86742',
 		londonId: '2643743',
 		weatherApi: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=18b3eeda7e71d2d3946ccfbfeea86742' 
+	},
+
+	moveOffice: function(x, y, speed) {
+		var top, left;
+		var office = this.ui.office;
+		var loc = office.getBoundingClientRect();
+
+		top = loc.top;
+		left = loc.left;
+
+		office.style.top = top + (speed * x); 
+		office.style.left = left + (speed * y); 
+
+		office.classList += ' has-moved';
+
+	},
+
+	setWind: function(deg, speed) {
+		var x, y;
+		
+		if (deg <= 90) { 
+			console.log('is ne');
+			x = -10;
+			y = 10;
+		} else if(deg <= 180) { 
+			console.log('is se');
+			x = 10;
+			y = 10;
+		} else if(deg <= 270) {
+			console.log('is sw');
+			x = 10;
+			y= -10;
+		} else if(deg <= 360) {
+			console.log('is nw');
+			x = -10;
+			y = -10;
+		}
+
+		this.moveOffice(x, y, speed);
 	},
 
 	setColour: function(temp) {
@@ -75,6 +115,10 @@ var art = {
 				console.log(weather);
 				var temp = Math.round(weather.main.temp);
 				self.setColour(temp);
+
+				var windDeg = weather.wind.deg;
+				var windSpeed = weather.wind.speed;
+				self.setWind(windDeg, windSpeed);
 			});	
 
 		} else { //if fetch is not supported, fallback to Ajax
@@ -82,6 +126,7 @@ var art = {
 			.done(function(response) {
 			  	var temp = Math.round(response.main.temp);
 				self.setColour(temp);
+				//TODO: wind
 			});
 		}
 	}
